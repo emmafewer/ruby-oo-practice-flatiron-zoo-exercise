826ac9fcc16bf9046ad2a5037bcc7901ec3625ee
emmafewer <emmafewer20@gmail.com>
2020-11-23 11:57:18 -0800

    done

--- run.rb
+++ run.rb
@@ -4,6 +4,13 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("Central Park Zoo", "Central Park")
+zoo2 = Zoo.new("Bronx Zoo", "Bronx")
+
+zebra = Animal.new("zebra", 300, "zz", zoo1)
+giraffe = Animal.new("giraffe", 600, "tall guy", zoo2)
+
+
 
 
 binding.pry
